bd2220f1f5ebcd3c2e1cc787b0fd1f28ed2801f8
miqueas <micahmccallum@gmail.com>
2020-12-26 23:01:01 -0500

    commit tree cb0e2b3e5758d08b6924b74896962bb0cfdf2516

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,16 @@
 class Pirate
+  attr_accessor :name, :weight, :height
+
+  @@all = []
+
+  def initialize(params)
+    @name, @height, @weight = params[:name], params[:height], params[:weight]
+    @@all << self
+  end
+
+  def self.all 
+    @@all 
+  end
+
+
 end
